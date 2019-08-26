14db8c8d3b1b19dafa348465982fa94c37f41169
Jakey <jakeystunts@gmail.com>
2019-08-26 13:05:47 -0700

    Added Search capability

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -6,13 +6,17 @@ import Navbar from './Navbar'
 export default props => {
 const [zip, setZip] = useState('')
 
+function searchIt() {
+    props.history.push('/search/' + zip)
+}
+
     return (
         <div id="landingWrapper">
             <div id="nav">
                 <Navbar/>
             </div>
             <div id="search">
-                <Form id="searchForm">
+                <Form id="searchForm" onSubmit={searchIt}>
                     <h1>Search your Zip Code for Garage sales in your area!</h1>
                     <input id="zip" type="text" value={zip} onChange={e => setZip(e.target.value)} />
                     <Button type="submit" primary>Search</Button>
