399637c96dcc0a2164fdd0b3f96e6a5eaff1a8c4
Sinar Christensen <sinarguzman@gmail.com>
2019-08-28 09:44:41 -0700

    some changed form validation

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -14,35 +14,72 @@ this.state= {
 
 
 }
+
 }
     
 render(){
 return(
 
    <div id="container1">
-     <p>Create Account</p>
-     <label htmlFor="name"> Name</label>
-       <input value={this.state.Name} name = "Name" id= "Name" type= "text"/>
+     <h2>Create Account</h2>
+     
+ <label htmlFor="name"> Name</label>
+  <input
+  type="text"
+  id="email"
+  name="email"
+  onChange={this.handleChange}
+  placeholder="Jacob"
+  className=""
+  />
        
-       <label htmlFor="username"> UserName</label>
-       <input value= {this.state.username}name = "username" id= "username" type= "text"/>
+       
+       
+  <label htmlFor="username">Username</label>
+  <input
+  type="text"
+  id="username"
+  name="username"
+  onChange={this.handleChange}
+  placeholder="jacob_jkl"
+ 
+  />
 
-       <label htmlFor="email"> Email</label>
-       <input value= {this.state.Email} name = "EMAIL" id= "Email" type= "text"/>
+<label htmlFor="email">Email</label>
+  <input
+  type="text"
+  id="email"
+  name="email"
+  onChange={this.handleChange}
+  placeholder="jacob@gmail.com"
+ 
+  />
        
-       <label htmlFor="Password"> Password</label>
-       <input name = "Password" id= "Password" type= "text"/>
+  <label htmlFor="Password"> Password</label>
+  <input
+  type="text"
+  id="password"
+  name="password"
+  onChange={this.handleChange}
+  placeholder="******"
+
+  />
        
-       <label htmlFor="confirmpassword"> Confirm Password</label>
-       <input name = "confirmpassword" id= "Cornfirmpassword" type= "text"/>
+  <label htmlFor="confirmpassword"> Confirm Password</label>
+  <input
+  type="text"
+  id="confirmpassword"
+  name="confirmpassword"
+  onChange={this.handleChange}
+  placeholder="******"
+  
+  />
        
-       <form onSubmit={this.handleSubmit}>
-        
-        {/* <input type="text" value={this.state.value} onChange={this.handleChange} /> */}
-        
-        <input type="submit" value="Submit" />
-        
+      <div id="Submit">
+      <form onSubmit={this.handleSubmit}>
+      <input id="submitButton" type="submit" value="Submit" />
       </form>
+      </div>
       
    </div> 
 )
@@ -54,6 +91,3 @@ return(
 export default FormApp
 
 
-{/* /* <label htmlFor="submit">Submit</label>
-        <input value={this.state.Submit} name = "Submit" id="Submit"type= "text"/>
-       */ }
