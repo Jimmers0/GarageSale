464dd4b24aa54c4e0e221f1d70a605e52824c7e7
Siny <sinyozz.developer@gmail.com>
2019-08-31 18:45:29 -0700

    More inventory

---
+++ client/src/actions/inventory.actions.js
@@ -0,0 +1,15 @@
+import store from '../store'
+import axios from 'axios'
+
+export function getInventory() {
+    
+    axios.post('/api/inventory', {
+        // pull userID 
+        userID: 1
+    }).then(resp => {
+        store.dispatch({
+            type: 'INVENTORY',
+            payload: resp.data.inventory
+          })
+    })
+}
--- client/src/components/Inventory.js
+++ client/src/components/Inventory.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
-export default props => {
 
-    return (<h1>Hello</h1>)
+export default props => {
+    
+    // pull inventory from redux
+    // map each inventory into a div container
+    return (<div><span>Inventory</span></div>)
 }
