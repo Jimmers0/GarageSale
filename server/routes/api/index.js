1748d914a34585427a6e642373a0f1357c4522a2
Jakey <jakeystunts@gmail.com>
2019-09-03 09:25:56 -0700

    Adding deleted code

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -86,6 +86,18 @@ router.post('/login', (req, res, next) => {
   });
 }
 })
+router.post('/createPost', (req, res, next) => {
+  var id = shortid.generate()
+  const sql = `INSERT INTO posts (name, date, active, user_id, zip, city, state, address, postID) VALUES (?,?,?,?,?,?,?,?,?)`
+  conn.query(sql, [req.body.name, req.body.date, true, req.body.user_id, req.body.zip, req.body.city, req.body.state, req.body.address, id], (err, result, fields) => {
+    req.body.images.forEach(item => {
+      const imageSQL = 'INSERT into items (price, picture, post_id) VALUES (?,?,?)'
+      conn.query(imageSQL, [item.price, item.url, id], (err, result, fields) => {
+      })
+    })
+    res.json({id: id})
+  })
+})
 
 
 module.exports = router
