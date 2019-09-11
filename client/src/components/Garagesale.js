ac9e086e321d6027227b5b340a58bfa43ef0c070
Jakey <jakeystunts@gmail.com>
2019-09-11 16:20:33 -0700

    LocalStorage login method

--- client/src/components/Garagesale.js
+++ client/src/components/Garagesale.js
@@ -4,6 +4,7 @@ import { getMySale } from '../actions/landing.actions'
 import '../styles/post.css'
 import moment from 'moment'
 import {Link} from 'react-router-dom'
+import {Button} from 'semantic-ui-react'
 
 
 export default props => {
@@ -19,20 +20,18 @@ export default props => {
         <div className="listingcontent">
             <div className="postcontainer">
             {garageSale.length === 0 ? <div className="error">You do not have any garage sales. Youre created garage sales will appear here! </div> : garageSale.map((sale, i) => (
-                <Link to={`/post/${sale.postID}`}>
-                <div key={'sale' + i} className="salepost">
-                    <div className="nameanddate">
-                    <strong>{sale.name}</strong> {moment(sale.date).format("dddd MM/DD")} 
-                    </div>
-                    <div>
-                    <p>City: {sale.city} State: {sale.state}</p>
-                    </div> 
-                    <div>
-                    <p>{sale.zip === props.match.params.zip ? "In your area" : `Distance: ${sale.distance}` }</p>
-                    <p>Address: {sale.address}</p>
-                    </div>
-                </div>
+                  <div key={'sale' + i} className="salepost">
+                  <h1>{sale.address}</h1>
+                  <p>{sale.city}, {sale.state} {sale.zip}</p>
+                  <p>Phone: (702) 937-8875</p>
+                  <p>Disance: {sale.distance} (About {sale.duration} away from your area)</p>
+                  <p>Date and Time: {moment(sale.date).format("dddd MM/DD")} from 10:00AM to 6:00PM</p>
+                  <div id="buttons">
+                  <Link to={`/post/${sale.postID}`}>
+                  <Button size="small" primary>View More</Button>
                 </Link>
+                </div>
+                </div>
             ))}
             </div>
         </div>
